bcadc05ad539a6e04511c5c57eefd70e4dae244c
Mathieu Kohl <mathieu.kohl@m4finvest.com>
2023-01-12 17:12:34 +0100

    modifications on data, and adding a transition box

--- src/App.vue
+++ src/App.vue
@@ -3,39 +3,33 @@
     <div class="list-container">
       <h2>Syllable</h2>
       <ul>
-        <draggable class="dragArea list-group w-full" :list="list1">
-          <li v-for="(item, index) in list1" v-bind:key="index">{{ item }}</li>
+        <draggable v-model="list1" @start="drag=true" @end="drag=false">
+          <li v-for="(item, index) in list1" v-bind:key="index">{{ item.name }}</li>
         </draggable>
       </ul>
     </div>
     <div class="list-container">
       <h2>JP Characters</h2>
       <ul>
-        <draggable class="dragArea list-group w-full" :list="list2">
-          <li v-for="(item, index) in list2" v-bind:key="index">{{ item }}</li>
+        <draggable v-model="list2" @start="drag=true" @end="drag=false">
+          <li v-for="(item, index) in list2" v-bind:key="index">{{ item.name }}</li>
         </draggable>
       </ul>
     </div>
-  </div>
-  <div class="container">
-    <div class="list-container">
-      <h2>Syllable</h2>
-      <ul>
-        <draggable class="dragArea list-group w-full" :list="list3">
-          <li v-for="(item, index) in list3" v-bind:key="index">{{ item }}</li>
-        </draggable>
-      </ul>
-    </div>
-    <div class="list-container">
-      <h2>JP Characters</h2>
-      <ul @drop="drop" @dragover.prevent>
-        <draggable class="dragArea list-group w-full" :list="list4">
-          <li v-for="(item, index) in list4" v-bind:key="index">{{ item }}</li>
-        </draggable>
-      </ul>
-    </div>
-  </div>
+</div>
+  
   <button class="btn_random" @click="randomize">Randomize</button>
+
+  <div class="box">
+      <div class="title">Transition Box</div>
+      <draggable
+        v-model="storage"
+        group="list-group"
+        class="draggable"
+      >
+      <li class="item" v-for="(item, index) in storage" v-bind:key="index">{{ item.name }}</li>
+      </draggable>
+  </div>
 </template>
 
 <script>
@@ -51,8 +45,7 @@ export default defineComponent({
     return {
       list1: data.list1,
       list2: data.list2,
-      list3: data.list3,
-      list4: data.list4,
+      storage: []
     }
   },
   mounted() {
@@ -67,9 +60,36 @@ export default defineComponent({
     randomize() {
       this.list1.sort(() => Math.random() - 0.5);
       this.list2.sort(() => Math.random() - 0.5);
-      this.list3.sort(() => Math.random() - 0.5);
-      this.list4.sort(() => Math.random() - 0.5);
+    }/*,
+    dragStart(event, item) {
+      event.dataTransfer.setData("text/plain", item);
+    },
+    dragEnd(event) {
+      event.dataTransfer.clearData();
+    },
+    drop(event) {
+      const data = event.dataTransfer.getData("text/plain");
+      const from = event.dataTransfer.getData("from");
+      this[from] = this[from].filter(i => i !== data);
+      this.storage.push(data);
     }
+    /*
+    drop(event) {
+      const data = event.dataTransfer.getData("text/plain");
+      const from = event.dataTransfer.getData("from");
+      const to = event.currentTarget.parentElement.previousElementSibling.textContent;
+
+      if(from === to) return;
+
+      // if the target is the storage-box
+      if(to === 'Storage') {
+        this.storage.push(data);
+        this[from] = this[from].filter(i => i !== data);
+      } else {
+        this[from] = this[from].filter(i => i !== data);
+        this[to].push(data);
+      }
+    }*/
   }
 })
 </script>
@@ -103,5 +123,34 @@ li {
   border: none;
   cursor: pointer;
   width: 100%;
+}
+
+.transfer-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.box {
+  border: 1px solid rgba(168, 168, 168, 0.274);
+  margin-bottom: 1em;
+  border-radius: 5px;
+}
+
+.box .title {
+  border-bottom: 1px solid  rgb(238, 238, 238);
+  text-align: center;
+  padding: 5px 0 5px 0;
+  background-color: rgba(250, 250, 251, 1);
+}
+.box .draggable {
+  width: 20em;
+  height: 10em;
+}
+
+.box .item {
+  background-color: lightblue;
+  border-bottom: 1px solid rgb(168, 168, 168);
+  cursor: default;
+  padding-left: 10px;
 }
 </style>
