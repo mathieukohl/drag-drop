a0cba69d2681cbd4f4a53ee71f59ac0abc458ea3
Mathieu Kohl <mathieu.kohl@m4finvest.com>
2023-01-12 11:19:35 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="container">
+      <div class="list-container">
+        <h2>Syllable</h2>
+        <ul>
+          <draggable class="dragArea list-group w-full" :list="list1" @change="log">
+            <li v-for="(item, index) in list1" v-bind:key="index" @dragstart="dragStart($event, item)" draggable>{{ item }}</li>
+          </draggable>
+        </ul>
+      </div>
+    <div class="list-container">
+      <h2>JP Characters</h2>
+      <ul @drop="drop" @dragover.prevent>
+        <draggable class="dragArea list-group w-full" :list="list2" @change="log">
+          <li v-for="(item, index) in list2" v-bind:key="index" @dragstart="dragStart($event, item)" draggable>{{ item }}</li>
+        </draggable>
+      </ul>
+    </div>
+    <button class="btn_random" @click="randomize">Randomize</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { VueDraggableNext } from 'vue-draggable-next';
+import data from './data.json'
+
+export default defineComponent({
+  components: {
+      draggable: VueDraggableNext,
+  },
+  data() {
+    return {
+      list1: data.list1,
+      list2: data.list2
+    }
+  },
+  mounted() {
+    fetch('data.json')
+      .then(response => response.json())
+      .then(data => {
+        this.list1 = data.list1;
+        this.list2 = data.list2;
+      });
+  },
+  methods: {
+    dragStart(event, item) {
+      event.dataTransfer.setData("text/plain", item);
+    },
+    drop(event) {
+      const data = event.dataTransfer.getData("text/plain");
+      const from = event.dataTransfer.getData("from");
+      const to = event.currentTarget.parentElement.previousElementSibling.textContent;
+
+      if(from === to) return;
+
+      this[from] = this[from].filter(i => i !== data);
+      this[to].push(data);
+    },
+    randomize() {
+      this.list1.sort(() => Math.random() - 0.5);
+      this.list2.sort(() => Math.random() - 0.5);
+    }
+  }
+})
+</script>
+
+<style>
+
+.list-container {
+  float: left;
+  width: 50%;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  padding: 10px;
+  border: 1px solid #ccc;
+  margin-bottom: 10px;
+  margin-right: 10px;
+  background-color: #f9f9f9;
+  cursor: move;
+}
+
+.btn_random{
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+</style>
